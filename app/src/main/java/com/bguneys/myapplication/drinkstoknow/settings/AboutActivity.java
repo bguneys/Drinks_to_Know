ca5ed500ba51b590370afd890ca8c48c5b481e8e
bguneys <42909556+bguneys@users.noreply.github.com>
2020-05-30 18:27:25 +0300

    AboutActivity added

---
+++ app/src/main/java/com/bguneys/myapplication/drinkstoknow/settings/AboutActivity.java
@@ -0,0 +1,21 @@
+package com.bguneys.myapplication.drinkstoknow.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.bguneys.myapplication.drinkstoknow.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
